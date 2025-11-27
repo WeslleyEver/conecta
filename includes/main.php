6cf26f8a1fa557a80499c51656304563d1e2e45a
WeslleyEver <weslley.git30@gmail.com>
2025-11-26 23:41:21 -0300

    Planos Adicionados

--- includes/main.php
+++ includes/main.php
@@ -48,6 +48,170 @@
 				</div>
 			</div>
 		</section>
+		<section id="plan">
+			<div class="container__section" id="plan-container">
+				<div class="box__info">
+					<h1 class="txt-c">
+						Planos <strong class="c-bl3">Conecta</strong>
+					</h1>
+				</div>
+				<div class="box__bottom d-flex">
+					<div class="cards-container">
+						<div class="box__card effect fade-up">
+							<div class="card">
+								<div class="card__title">
+									<h1>Plano Código</h1>
+									<p>
+										Ideal para quem está começando sua jornada profissional.
+									</p>
+									<h2>R$49 <span>/mês</span></h2>
+									<button class="card-button"
+										data-url="https://app.beanmusicpromotion.com/releases/checkout?subscribe=1">Quero
+										esse plano</button>
+									<small>
+										Perfeito para quem quer dar o primeiro passo com segurança e começar a construir experiência real.
+									</small>
+								</div>
+							</div>
+							<div class="card__bottom">
+								<ul>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Acesso aos cursos básicos da plataforma
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Suporte online para dúvidas
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Certificados de conclusão
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Acesso a vagas em empresas iniciantes e de pequeno porte
+									</li>
+								</ul>
+								<span class="show-more">+ Saiba mais</span>
+							</div>
+						</div>
+						<div class="box__card effect fade-up" data-delay=".3s">
+							<div class="card">
+								<div class="card__title">
+									<h1>Plano Avançado</h1>
+									<p>
+										Para quem já quer dar o próximo passo e se destacar.
+									</p>
+									<h2>R$69 <span>/mês</span></h2>
+									<button class="card-button"
+										data-url="https://app.beanmusicpromotion.com/releases/checkout?subscribe=2">Quero
+										esse plano</button>
+									<small>
+										Seu perfil ganha mais visibilidade, ampliando suas chances em empresas maiores.
+									</small>
+								</div>
+							</div>
+							<div class="card__bottom">
+								<ul>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Acesso a cursos intermediários e avançados
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Trilhas de carreira guiadas
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Conteúdos exclusivos com profissionais e mentores renomados
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Certificados de conclusão
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Acesso a vagas em empresas de médio porte e marcas reconhecidas no mercado
+									</li>
+								</ul>
+								<span class="show-more">+ Saiba mais</span>
+							</div>
+						</div>
+						<div class="box__card effect fade-up" data-delay=".5s">
+							<div class="card">
+								<div class="card__title">
+									<h1>Plano Plus</h1>
+									<p>
+										Para quem busca crescimento acelerado e grandes oportunidades.
+									</p>
+									<h2>R$99 <span>/mês</span></h2>
+									<button class="card-button"
+										data-url="https://app.beanmusicpromotion.com/releases/checkout?subscribe=3"
+										class="card-button">Quero esse plano</button>
+									<small>
+										O plano ideal para quem quer se destacar, ser visto por grandes empresas e acelerar a carreira.
+									</small>
+								</div>
+							</div>
+							<div class="card__bottom">
+								<ul>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Acesso total a todos os cursos e trilhas de carreira
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Destaque no ranking de talentos Conecta
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Certificações exclusivas e prioritárias
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Mentorias personalizadas com acompanhamento individual
+									</li>
+									<li>
+										<div class="">
+											<div class="check-circle"></div>
+										</div>
+										Acesso direto a vagas em grandes empresas, multinacionais e startups de alto impacto
+									</li>
+								</ul>
+								<span class="show-more">+ Saiba mais</span>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
 		<section id="faq" class="faq">
 			<div class="container__section">
 				<h1 class="c-bl2">Dúvidas Frequentes</h1>
--- js/main.js
+++ js/main.js
@@ -1,48 +1,59 @@
-const hamburger = document.getElementById('hamburger');
-const nav = document.getElementById('nav');
-const header = document.getElementById('header');
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+const header = document.getElementById("header");
 
-
-hamburger.addEventListener('click', () => {
-nav.classList.toggle('open');
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("open");
 });
 
-
-window.addEventListener('scroll', () => {
-if (window.scrollY > 10) {
-header.classList.add('scrolled');
-} else {
-header.classList.remove('scrolled');
-}
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
 // Ajustar padding do body dinamicamente conforme altura do header
 function ajustarPadding() {
-const headerHeight = header.offsetHeight;
-document.body.style.paddingTop = headerHeight + 'px';
+  const headerHeight = header.offsetHeight;
+  document.body.style.paddingTop = headerHeight + "px";
 }
 
-
-window.addEventListener('resize', ajustarPadding);
-window.addEventListener('load', ajustarPadding);
+window.addEventListener("resize", ajustarPadding);
+window.addEventListener("load", ajustarPadding);
 ajustarPadding();
 
- const faqItems = document.querySelectorAll(".faq-item");
+const faqItems = document.querySelectorAll(".faq-item");
 
-  faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
 
-    question.addEventListener("click", () => {
-      faqItems.forEach((i) => {
-        if (i !== item) {
-          i.classList.remove("active");
-        }
-      });
-      item.classList.toggle("active");
+  question.addEventListener("click", () => {
+    faqItems.forEach((i) => {
+      if (i !== item) {
+        i.classList.remove("active");
+      }
     });
+    item.classList.toggle("active");
   });
+});
 
-  document.addEventListener("click", (e) => {
-    if (!e.target.closest(".faq")) {
-      faqItems.forEach((i) => i.classList.remove("active"));
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".faq")) {
+    faqItems.forEach((i) => i.classList.remove("active"));
+  }
+});
+
+document.querySelectorAll(".show-more").forEach((btn_card) => {
+  btn_card.addEventListener("click", () => {
+    const ul = btn_card.previousElementSibling; // pega o <ul>
+    ul.classList.toggle("show-all");
+
+    // troca o texto do botão
+    if (ul.classList.contains("show-all")) {
+      btn_card.textContent = "- Mostrar menos";
+    } else {
+      btn_card.textContent = "+ Saiba mais";
     }
   });
+});
